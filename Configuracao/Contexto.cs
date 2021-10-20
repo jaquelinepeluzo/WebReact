c011216cafb2af28b4638fa36ccbff8c9d4e83b3
Jaqueline Peluzo <49986430+jaquelinepeluzo@users.noreply.github.com>
2021-10-20 17:07:18 -0300

    Add files via upload

---
+++ Configuracao/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebJaqueReact.Model;
+
+namespace WebJaqueReact.Configuracao
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
---
+++ Model/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebJaqueReact.Model
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+        
+        [Column("Descricao")]
+        public string Descricao { get; set; }
+    }
+}
